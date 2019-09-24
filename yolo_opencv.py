b2edce2e2fb3ee9c2608cc8d9f9fe11a9c9a4ea2
NSiwar <44530468+siwarnasri@users.noreply.github.com>
2019-09-24 09:58:40 +0100

    Add files via upload

---
+++ yolo_opencv.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+import numpy as np
+import argparse
+import time
+import cv2
+import os
+ 
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to input image")
+ap.add_argument("-y", "--yolo", required=True, help="base path to YOLO directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3, help="threshold when applying non-maxima suppression")
+args = vars(ap.parse_args())
+
+labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+ 
+# initialize a list of colors to represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
+
+weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
+configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+ 
+# load our YOLO object detector trained on COCO dataset (80 classes)
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+image = cv2.imread(args["image"])
+(H, W) = image.shape[:2]   
+    
+# determine only the *output* layer names that we need from YOLO
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+ 
+# construct a blob from the input image and then perform a forward
+# pass of the YOLO object detector, giving us our bounding boxes and
+# associated probabilities
+blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+net.setInput(blob)
+start = time.time()
+layerOutputs = net.forward(ln)
+end = time.time()
+ 
+# show timing information on YOLO
+print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+# initialize our lists of detected bounding boxes, confidences, and
+# class IDs, respectively
+boxes = []
+confidences = []
+classIDs = []
+
+# loop over each of the layer outputs
+for output in layerOutputs:
+	# loop over each of the detections
+	for detection in output:
+		# extract the class ID and confidence (i.e., probability) of
+		# the current object detection
+		scores = detection[5:]
+		classID = np.argmax(scores)
+		confidence = scores[classID]
+ 
+		# filter out weak predictions by ensuring the detected
+		# probability is greater than the minimum probability
+		if confidence > args["confidence"]:
+			# scale the bounding box coordinates back relative to the
+			# size of the image, keeping in mind that YOLO actually
+			# returns the center (x, y)-coordinates of the bounding
+			# box followed by the boxes' width and height
+			box = detection[0:4] * np.array([W, H, W, H])
+			(centerX, centerY, width, height) = box.astype("int")
+ 
+			# use the center (x, y)-coordinates to derive the top and
+			# and left corner of the bounding box
+			x = int(centerX - (width / 2))
+			y = int(centerY - (height / 2))
+ 
+			# update our list of bounding box coordinates, confidences,
+			# and class IDs
+			boxes.append([x, y, int(width), int(height)])
+			confidences.append(float(confidence))
+			classIDs.append(classID)
+            
+# import required packages
+import cv2
+import argparse
+import numpy as np
+
+
+# handle command line arguments
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True, help = 'path to input image')
+ap.add_argument('-c', '--config', required=True, help = 'path to yolo config file')
+ap.add_argument('-w', '--weights', required=True, help = 'path to yolo pre-trained weights')
+ap.add_argument('-cl', '--classes', required=True, help = 'path to text file containing class names')
+args = ap.parse_args()
+
+# read input image
+image = cv2.imread(args.image)
+
+Width = image.shape[1]
+Height = image.shape[0]
+scale = 0.00392
+
+# read class names from text file
+classes = None
+with open(args.classes, 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+# generate different colors for different classes 
+COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# read pre-trained model and config file
+net = cv2.dnn.readNet(args.weights, args.config)
+
+# create input blob 
+blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+
+# set input blob for the network
+net.setInput(blob)
+# function to get the output layer names 
+# in the architecture
+def get_output_layers(net):
+    
+    layer_names = net.getLayerNames()
+    
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+# function to draw bounding box on the detected object with class name
+def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+
+    label = str(classes[class_id])
+
+    color = COLORS[class_id]
+
+    cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
+
+    cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+   
+# run inference through the network
+# and gather predictions from output layers
+outs = net.forward(get_output_layers(net))
+
+# initialization
+class_ids = []
+confidences = []
+boxes = []
+conf_threshold = 0.5
+nms_threshold = 0.4
+
+# for each detetion from each output layer 
+# get the confidence, class id, bounding box params
+# and ignore weak detections (confidence < 0.5)
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:
+            center_x = int(detection[0] * Width)
+            center_y = int(detection[1] * Height)
+            w = int(detection[2] * Width)
+            h = int(detection[3] * Height)
+            x = center_x - w / 2
+            y = center_y - h / 2
+            class_ids.append(class_id)
+            confidences.append(float(confidence))
+            boxes.append([x, y, w, h])
+           
+# apply non-max suppression
+indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+# go through the detections remaining
+# after nms and draw bounding box
+for i in indices:
+    i = i[0]
+    box = boxes[i]
+    x = box[0]
+    y = box[1]
+    w = box[2]
+    h = box[3]
+    
+    draw_bounding_box(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+
+# display output image    
+cv2.imshow("object detection", image)
+
+# wait until any key is pressed
+cv2.waitKey()
+    
+ # save output image to disk
+cv2.imwrite("object-detection.jpg", image)
+
+# release resources
+cv2.destroyAllWindows()
